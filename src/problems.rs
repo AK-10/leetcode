a013da65e1631241686c1e741e0645f7ca75788d
AK-10 <atsushi524k10@gmail.com>
2022-09-04 20:16:00 +0900

    solve q0206 reverse linked list

--- src/problems.rs
+++ src/problems.rs
@@ -3,3 +3,4 @@ pub mod q0002_add_two_numbers;
 pub mod q0003_longest_subustring_without_repeating_characters;
 pub mod q0083_remove_duplicates_from_sorted_list;
 pub mod q0020_valid_parentheses;
+pub mod q0206_reverse_linked_list;
---
+++ src/problems/q0206_reverse_linked_list.rs
@@ -0,0 +1,74 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution {}
+impl Solution {
+    #[allow(unused)]
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Solution::rec_rev(None, head)
+    }
+
+    fn rec_rev(prev: Option<Box<ListNode>>, cur: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match cur {
+            Some(mut node) => {
+                let next = node.next;
+                node.next = prev;
+                Solution::rec_rev(Some(node), next)
+            }
+            None => prev
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::problems::q0206_reverse_linked_list::ListNode;
+    use crate::problems::q0206_reverse_linked_list::Solution;
+
+    #[test]
+    fn test() {
+        let list = None;
+        assert_eq!(Solution::reverse_list(list), None);
+
+        let list = Some(Box::new(ListNode { val: 1, next: None }));
+        let expected = Some(Box::new(ListNode { val: 1, next: None }));
+        assert_eq!(Solution::reverse_list(list), expected);
+
+        let list = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode { val: 2, next: None })),
+        }));
+        let expected = Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode { val: 1, next: None })),
+        }));
+        assert_eq!(Solution::reverse_list(list), expected);
+
+        let list = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 3, next: None })),
+            })),
+        }));
+        let expected = Some(Box::new(ListNode {
+            val: 3,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 1, next: None })),
+            })),
+        }));
+        assert_eq!(Solution::reverse_list(list), expected);
+    }
+}
